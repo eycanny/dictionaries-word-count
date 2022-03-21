9a22e9a9243f371af0f0b3734054d2f79200c91c
eycanny <nancy.the.coder@gmail.com>
2022-03-21 13:00:24 -0700

    Created tokenize() and count_words()

--- wordcount.py
+++ wordcount.py
@@ -3,20 +3,34 @@ import sys
 
 input_file = open(sys.argv[1])
 
-# def word_count(input_file):
 
-word_counts = {}
-    
-    # file = open(input_file)
-    
-for line in input_file:
-    line = line.rstrip()
-    word_list = line.split(" ")
+def tokenize(file_name=input_file):
 
-    for word in word_list:
-        word_counts[word] = word_counts.get(word,0) + 1
-    
-input_file.close()
+    words = []  
+    for line in file_name:
+        line = line.rstrip()
+        word_list = line.split(" ")
 
-for word in word_counts:
-    print(word, word_counts[word])
+        for word in word_list:
+            if word not in words:
+                words.append(word)
+
+    return words
+
+def count_words(words):
+    """Take in a list of strings and return a dictionary of each string and the number 
+    of times it appears in the list.
+    
+    """
+    
+    count_words = {}
+
+    for word in words:
+        count_words[word] = count_words.get(word,0) + 1
+
+    return count_words
+    
+# input_file.close()
+
+# for word in word_counts:
+#     print(word, word_counts[word])
